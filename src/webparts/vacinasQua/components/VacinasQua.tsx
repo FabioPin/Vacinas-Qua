fdf0e8c12cd8632e57ef7d0ccfbd34c4abcf1f09
FabioPin <fajos.pinheiro@hotmail.com>
2021-05-29 22:20:56 +0100

    react Component fetch

--- src/webparts/vacinasQua/components/VacinasQua.tsx
+++ src/webparts/vacinasQua/components/VacinasQua.tsx
@@ -4,6 +4,18 @@ import { IVacinasQuaProps } from './IVacinasQuaProps';
 import { escape } from '@microsoft/sp-lodash-subset';
 
 export default class VacinasQua extends React.Component<IVacinasQuaProps, {}> {
+
+  public componentDidMount() {
+    fetch('http://localhost:5000/todos')
+      .then(
+        (result) => {
+          this.setState({
+            
+          });
+        }
+      );
+  }
+
   public render(): React.ReactElement<IVacinasQuaProps> {
     return (
       <div className={ styles.vacinasQua }>
@@ -12,7 +24,7 @@ export default class VacinasQua extends React.Component<IVacinasQuaProps, {}> {
             <div className={ styles.column }>
               <span className={ styles.title }>Welcome to SharePoint!</span>
               <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
+              <p className={ styles.description }></p>
               <a href="https://aka.ms/spfx" className={ styles.button }>
                 <span className={ styles.label }>Learn more</span>
               </a>
